2ee795b3bb6cf6f84a6c9f0565e510222d279a27
Law <3220352981@qq.com>
2026-08-19 21:26:14 +0800

    refactor(test): merge ring queue benchmark into test file

--- idle_ring_queue_test.go
+++ idle_ring_queue_test.go
@@ -281,3 +281,26 @@ func TestPoolWithRingQueuePanicRecovery(t *testing.T) {
 
 	assert.Equal(t, 1000, sum)
 }
+
+// BenchmarkRingQueue_RemoveExpired measures the allocation count of
+// RemoveExpired with a mix of expired and alive workers.
+func BenchmarkRingQueue_RemoveExpired(b *testing.B) {
+	now := time.Now()
+	expiry := 5 * time.Second
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		rq := newRingQueue()
+		for j := 0; j < 1000; j++ {
+			if j%2 == 0 {
+				rq.Add(NewWorker(now.Add(-10 * time.Second))) // expired
+			} else {
+				rq.Add(NewWorker(now.Add(-1 * time.Second))) // alive
+			}
+		}
+		b.StartTimer()
+
+		rq.RemoveExpired(now, expiry)
+	}
+}
